e5b599d70135e09a22ab69565f880dd32f171105
John Mitsch <jomitsch@redhat.com>
2015-06-16 14:00:42 -0400

    removed pr hack

--- repo-setup.sh
+++ repo-setup.sh
@@ -4,5 +4,3 @@ git remote add upstream git@github.com:Katello/katello.git
 git fetch upstream
 git remote add fork git@github.com:johnpmitsch/katello.git
 git fetch fork
-cp katello_git_config ~/katello/.git/config
-git fetch origin
