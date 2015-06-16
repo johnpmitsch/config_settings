3cf746183c62d66284fbcd2523f287daa9809e91
John Mitsch <jomitsch@redhat.com>
2015-06-16 13:35:11 -0400

    added github ssh setup

--- setup.sh
+++ setup.sh
@@ -6,5 +6,12 @@ cp .vimrc_copy ~/.vimrc
 cp .bashrc_copy ~/.bashrc
 cp .gitconfig_copy ~/.gitconfig
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-'gem "puma"' >> ~/foreman/Gemfile
+echo 'gem "puma"' >> ~/foreman/Gemfile
 cp config.ru ~/foreman/config.ru
+echo "setting up ssh for github"
+ssh-keygen -t rsa -b 4096 -C "jomitsch@redhat.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub
+echo "copy and paste that to github ssh keys"
+
