9e50dab8735b5e1fcad2ab81881dd0626bc65264
John Mitsch <jomitsch@redhat.com>
2015-07-08 09:57:27 -0400

    added ktests

--- repo-setup.sh
+++ repo-setup.sh
@@ -4,3 +4,5 @@ git remote add upstream git@github.com:Katello/katello.git
 git fetch upstream
 git remote add fork git@github.com:johnpmitsch/katello.git
 git fetch fork
+cp ~/config_settings/ktest ~/foreman
+cp ~/config_settings/ktest-live ~/foreman 
