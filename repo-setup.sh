26028c6c3b2b55fdd29d13304dfe7fcb1cae1dd0
John Mitsch <jomitsch@redhat.com>
2015-08-04 20:56:35 -0400

    fixed typo

--- repo-setup.sh
+++ repo-setup.sh
@@ -4,7 +4,7 @@ git remote add upstream git@github.com:Katello/katello.git
 git fetch upstream
 git remote add fork git@github.com:johnpmitsch/katello.git
 git fetch fork
-cp ~/config-settings/pre-push ~/katello/.git/hooks
-cp ~/config-settings/pre-push ~/foreman/.git/hooks
+cp ~/config_settings/pre-push ~/katello/.git/hooks
+cp ~/config_settings/pre-push ~/foreman/.git/hooks
 cp ~/config_settings/ktest ~/foreman
 cp ~/config_settings/ktest-live ~/foreman 
