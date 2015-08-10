7a710ed09e1ab3c7780e16fc81f1ede713bf51dd
John Mitsch <jomitsch@redhat.com>
2015-08-10 13:00:04 -0400

    added screenrc file and bundle install

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@ echo "copying files"
 cp .vimrc_copy ~/.vimrc
 cp .bashrc_copy ~/.bashrc
 cp .gitconfig_copy ~/.gitconfig
+cp .screenrc_copy ~/.screenrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 echo 'gem "puma"' >> ~/foreman/Gemfile
 cp config.ru ~/foreman/config.ru
+cd ~/foreman
+bundle install
 
