73c01dea6b7c100458344b27b75cf1d7c9a0e38b
John Mitsch <jomitsch@redhat.com>
2015-09-21 14:19:55 -0400

    add ssh key to github using curl

--- set_up_ssh.sh
+++ set_up_ssh.sh
@@ -1,7 +1,10 @@
 echo "setting up ssh for github"
 ssh-keygen -t rsa -b 4096 -C "jomitsch@redhat.com"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub
-echo "copy and paste that to github ssh keys"
-
+NEW_KEY="$(cat ~/.ssh/id_rsa.pub)"
+echo "$(echo $NEW_KEY)"
+echo "copying and pasting that to github ssh keys"
+curl -u "johnpmitsch" --data '{"title":"test-key","key":"'"$NEW_KEY"'"}' https://api.github.com/user/keys
+eval `ssh-agent`
+ssh-add
+./setup.sh
+./repo-setup.sh
