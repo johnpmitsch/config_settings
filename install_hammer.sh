254b5e41d6146d40a09819fcfda21540289d918b
John Mitsch <jomitsch@redhat.com>
2015-10-13 10:04:55 -0400

    added hammer install script

---
+++ install_hammer.sh
@@ -0,0 +1,15 @@
+cd ~
+git clone https://github.com/Katello/hammer-cli-katello.git
+git clone https://github.com/theforeman/hammer-cli.git
+git clone https://github.com/theforeman/hammer-cli-foreman.git
+git clone https://github.com/theforeman/hammer-cli-foreman-tasks.git
+cd ~/hammer-cli-katello
+echo "1.9.3" > .ruby-version
+echo "hammer" > .ruby-gemset
+cd ..; cd -
+bundle install
+cp ~/config_settings/hammer_gemfile.txt Gemfile.local
+mkdir -p ~/.hammer/cli.modules.d
+cp ~/config_settings/foreman-copy.yml ~/.hammer/cli.modules.d/foreman.yml
+cp ~/config_settings/katello-local.yml ~/.hammer/cli.modules.d/katello.yml
+bundle exec hammer -vh
