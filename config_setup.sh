1b1c7fd2d42735e6b98224cf89b0055dd9b5d5e6
John Mitsch <jomitsch@redhat.com>
2017-04-05 14:30:49 -0400

    symlinked gitconfig

--- config_setup.sh
+++ config_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "setting up dev environment"
 echo "installing packages"
-sudo yum install -y vim fpaste
+sudo yum install -y vim fpaste tig
 echo "copying files"
 rm -f ~/.bashrc
 ln -s /home/vagrant/config_settings/.vimrc_copy ~/.vimrc
 ln -s /home/vagrant/config_settings/.bashrc_copy ~/.bashrc
 ln -s /home/vagrant/config_settings/.pryrc_copy ~/.pryrc
+ln -s /home/vagrant/config_settings/.gitconfig_copy ~/.gitconfig
 export PS1="\[\e[1;$(shuf -i31-36 -n1)m\][\u@\h \W\$(git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/{\1}/')]\$\[\e[0m\] "
-cp .gitconfig_copy ~/.gitconfig
 cp .screenrc_copy ~/.screenrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
@@ -17,4 +17,3 @@ echo 'gem "pry"' >> ~/foreman/bundler.d/katello.local.rb
 echo 'gem "pry-byebug"' >> ~/foreman/bundler.d/katello.local.rb
 cd ~/foreman
 bundle install
-
