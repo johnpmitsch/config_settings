f2d7ccf83bec562c4cba4bd491197bd42747b119
John Mitsch <jomitsch@redhat.com>
2018-04-09 12:20:51 -0400

    Update repo-setup.sh

--- repo-setup.sh
+++ repo-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ~/katello
 git remote add upstream git@github.com:Katello/katello.git
-git fetch upstream
+git remote add origin https://github.com/Katello/katello.git
 git remote add fork git@github.com:johnpmitsch/katello.git
-git fetch fork
+git fetch -a
 cp ~/config_settings/pre-push ~/katello/.git/hooks
 cp ~/config_settings/pre-push ~/foreman/.git/hooks
 cp ~/config_settings/ktest ~/foreman
