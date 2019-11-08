de30b833660a330795a1b96a5f613cc1f2279f34
John Mitsch <jomitsch@redhat.com>
2019-11-08 10:32:26 -0500

    Move cd in script

--- setup_scripts/repo-setup.sh
+++ setup_scripts/repo-setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-cd ~/katello
-git remote add fork git@github.com:johnpmitsch/katello.git
-#git fetch -a
 cp ./pre-push ~/katello/.git/hooks
 cp ./pre-push ~/foreman/.git/hooks
 cp ./ktest ~/foreman
 cp ./ktest-live ~/foreman 
 echo "ktest*" >> ~/foreman/.git/info/exclude
+
+cd ~/katello
+git remote add fork git@github.com:johnpmitsch/katello.git
+#git fetch --all
+
 cd ~/foreman
 git remote add fork git@github.com:johnpmitsch/foreman.git
 echo "DONE"
