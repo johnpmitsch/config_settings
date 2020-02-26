a831313399604cf63b0933cb577d2e45cc7dd681
John Mitsch <jomitsch@redhat.com>
2020-02-26 10:11:48 -0500

    Update repo-setup.sh

--- setup_scripts/repo-setup.sh
+++ setup_scripts/repo-setup.sh
@@ -7,8 +7,10 @@ echo "ktest*" >> ~/foreman/.git/info/exclude
 
 cd ~/katello
 git remote add fork git@github.com:johnpmitsch/katello.git
+git remote add origin https://github.com/katello/katello.git
 #git fetch --all
 
 cd ~/foreman
 git remote add fork git@github.com:johnpmitsch/foreman.git
+git remote add origin https://github.com/foreman/foreman.git
 echo "DONE"
